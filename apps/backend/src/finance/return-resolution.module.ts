ce9b4e02516b3b4743586e7d447f8019dbc6bd9f
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-07-18 23:23:28 +0530

    S10-08 register admin return decisions

--- apps/backend/src/finance/return-resolution.module.ts
+++ apps/backend/src/finance/return-resolution.module.ts
@@ -1,16 +1,35 @@
 import { Module } from '@nestjs/common';
 
+import { AdminReturnDecisionController } from './admin-return-decision.controller';
+import { SupabaseAdminReturnDecisionGateway } from './admin-return-decision.gateway';
+import { AdminReturnDecisionService } from './admin-return-decision.service';
 import { MerchantReturnController } from './merchant-return.controller';
 import { SupabaseMerchantReturnGateway } from './merchant-return.gateway';
 import { MerchantReturnService } from './merchant-return.service';
-import { MERCHANT_RETURN_GATEWAY } from './return-resolution.tokens';
+import {
+  ADMIN_RETURN_DECISION_GATEWAY,
+  MERCHANT_RETURN_GATEWAY,
+} from './return-resolution.tokens';
 
 @Module({
-  controllers: [MerchantReturnController],
+  controllers: [MerchantReturnController, AdminReturnDecisionController],
   providers: [
     MerchantReturnService,
-    { provide: MERCHANT_RETURN_GATEWAY, useClass: SupabaseMerchantReturnGateway },
+    AdminReturnDecisionService,
+    {
+      provide: MERCHANT_RETURN_GATEWAY,
+      useClass: SupabaseMerchantReturnGateway,
+    },
+    {
+      provide: ADMIN_RETURN_DECISION_GATEWAY,
+      useClass: SupabaseAdminReturnDecisionGateway,
+    },
   ],
-  exports: [MERCHANT_RETURN_GATEWAY, MerchantReturnService],
+  exports: [
+    MERCHANT_RETURN_GATEWAY,
+    ADMIN_RETURN_DECISION_GATEWAY,
+    MerchantReturnService,
+    AdminReturnDecisionService,
+  ],
 })
 export class ReturnResolutionModule {}
