6ea981c37c99a8d95528239ca5f2b24cb842c7a2
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-07-21 16:43:08 +0530

    FE-S02-04 feat(ui): export shared primitives

---
+++ packages/ui-primitives/src/index.ts
@@ -0,0 +1,7 @@
+export * from './adapters.js';
+export * from './button.js';
+export * from './display.js';
+export * from './feedback.js';
+export * from './field.js';
+export * from './overlays.js';
+export * from './types.js';
