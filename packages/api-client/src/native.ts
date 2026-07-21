b19c776189eed8bfa9a398582712479e0082ba07
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-07-22 02:25:54 +0530

    feat(api-client): add native runtime entry point

---
+++ packages/api-client/src/native.ts
@@ -0,0 +1,21 @@
+export {
+  createApiClient,
+  type ApiClient,
+  type ApiClientOptions,
+  type RequestOptions,
+} from './client';
+export type {
+  AbortSignalLike,
+  AccessTokenProvider,
+  ActorType,
+  ApiClientLogEvent,
+  ApiClientLogger,
+  ApiClientResponse,
+  ApiErrorKind,
+  FetchLike,
+  FetchRequestInitLike,
+  FetchResponseLike,
+  HeadersLike,
+  NormalizedApiError,
+  RequestIdProvider,
+} from './types';
