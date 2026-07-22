0876cad7176faf94a706e56d9419b925278c6324
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-07-22 09:36:39 +0530

    BE-FE-001 feat(profile): register customer profile capability

--- apps/backend/src/me/me.module.ts
+++ apps/backend/src/me/me.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { CustomerProfileController } from './customer-profile.controller';
+import { SupabaseCustomerProfileGateway } from './customer-profile.gateway';
+import { CustomerProfileService } from './customer-profile.service';
+import { CUSTOMER_PROFILE_GATEWAY } from './customer-profile.tokens';
 import { DeviceRegistrationController } from './device-registration.controller';
 import { SupabaseDeviceRegistrationGateway } from './device-registration.gateway';
 import { DeviceRegistrationService } from './device-registration.service';
@@ -10,13 +14,18 @@ import { MeService } from './me.service';
 import { ME_GATEWAY } from './me.tokens';
 
 @Module({
-  controllers: [MeController, DeviceRegistrationController],
+  controllers: [MeController, CustomerProfileController, DeviceRegistrationController],
   providers: [
     MeService,
+    CustomerProfileService,
     DeviceRegistrationService,
     {
       provide: ME_GATEWAY,
       useClass: SupabaseMeGateway,
+    },
+    {
+      provide: CUSTOMER_PROFILE_GATEWAY,
+      useClass: SupabaseCustomerProfileGateway,
     },
     {
       provide: DEVICE_REGISTRATION_GATEWAY,
